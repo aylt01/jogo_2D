8c526936015fa1487e30ae30151edeca89f73373
juliagnaty <ailtonfotos2@gmail.com>
2026-09-08 12:10:48 -0300

    movimentação do player
    foi colocado a movimentaçao lateral do nootbook

---
+++ Assets/script/playermove.cs
@@ -0,0 +1,23 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    public float speed = 5f;
+    public float horizontal;
+
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        transform.position  += Vector3.right * horizontal * speed  *  Time.deltaTime ;
+       
+    }
+}
